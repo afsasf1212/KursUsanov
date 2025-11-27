a0406dd61fcee17b5283fc09ed480ff9af9a2d29
Глеб Ачуров <achurovgg@mail.ru>
2025-11-27 07:27:08 +0300

    создал класс

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Class1
+    {
+        int d = 50;
+    }
+}
